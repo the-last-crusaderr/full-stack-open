a1665ed7ea010760a22c5fcf0435e45fddffde3e
the-last-crusaderr <guptasumit922@gmail.com>
2024-07-03 18:09:20 +0530

    Refactor the data structure into an objects

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -46,19 +46,19 @@ function App() {
  console.log("Constructing pages with the help of Vite");
   
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = { name: 'Fundamentals of React',
+                  exercises1: 10}
+  const part2 = { name: 'Using props to pass data',
+                  exercises2 : 7}
+  const part3 = { name: 'State of a component',
+                  exercises3 : 14}
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3}
-        exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/> 
+      <Content part1={part1.name} part2={part2.name} part3={part3.name}
+        exercises1={part1.exercises1} exercises2={part2.exercises2} exercises3={part3.exercises3}/>
+      <Total exercises1={part1.exercises1} exercises2={part2.exercises2} exercises3={part3.exercises3}/> 
     </div>
   )
 }
